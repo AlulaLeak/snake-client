4b3d12df3e8786a2805e30e42a06098fdea545cb
AlulaLeak <alula.leakemariam94@gmail.com>
2021-12-02 03:38:19 +0000

    working movement

--- input.js
+++ input.js
@@ -1,18 +1,34 @@
-const net = require("net");
 
-const setupInput = function () {
+
+let connection;
+
+const setupInput = function(conn) {
+  
+  connection = conn; // link from play.js
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => handleUserInput(key));
   return stdin;
 };
 
-const handleUserInput = function (data) {
+const handleUserInput = function(data) {
+
+
   if (data === 'j') {
     process.exit();
+  } else if (data === 'w') {
+    connection.write("Move: up");
+  } else if (data === 's') {
+    connection.write("Move: down");
+  } else if (data === 'a') {
+    connection.write("Move: left");
+  } else if (data === 'd') {
+    connection.write("Move: right");
   }
 };
 
-module.exports = setupInput;
+module.exports = {
+  setupInput
+};
